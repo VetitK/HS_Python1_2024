ea96278d41f6a2a759159a0bd863637c9c009b04
VetitK <gmvetit20@gmail.com>
2024-09-10 19:58:16 +0700

    day2

--- day2.py
+++ day2.py
@@ -1,12 +1,4 @@
-print("hello world!")
-
-
-def fibo(n):
-    if n == 1 or n == 2:
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
-
-print(fibo(40))
-
-
+"""
+multiline comment
+multiline comment2
+"""
