138b2c8dc1855cd321fcd1bc354a2606c33d56f8
VetitK <gmvetit20@gmail.com>
2024-09-12 20:16:40 +0700

    day4 in-class work

---
+++ day4.py
@@ -0,0 +1,60 @@
+import turtle
+
+turtle.speed(1)
+turtle.shape('turtle')
+
+# length = 1000
+
+# turtle.forward(length)
+# turtle.left(90) # turn left 90
+# turtle.forward(length)
+# turtle.left(90)
+# turtle.forward(length)
+# turtle.left(90)
+# turtle.forward(length)
+# turtle.left(90)
+
+# turtle.forward(200)
+# turtle.right(90)
+# turtle.forward(100)
+
+# turtle.backward(100)
+
+# turtle.color('red')
+
+# turtle.backward(100)
+
+# turtle.forward(100)
+
+# turtle.penup()
+
+# turtle.forward(100)
+
+# turtle.pendown()
+# turtle.forward(100)
+
+# turtle.width(3)
+
+# turtle.color('violet')
+
+# turtle.forward(100)
+
+# turtle.circle(100, 180)
+
+turtle.color('purple', 'green')
+
+turtle.begin_fill()
+
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(60)
+turtle.right(120)
+turtle.forward(100)
+
+turtle.end_fill()
+
+# turtle.home()
+
+turtle.mainloop()
